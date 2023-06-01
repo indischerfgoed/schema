94a7f8bfe67f672acd3835cd1c652a7b23f68fe1
Olivier Jansen <olivier@spinque.com>
2023-06-01 22:05:22 +0200

    initial rough draft of doc generation script

---
+++ software/application_profile.py
@@ -0,0 +1,65 @@
+import uuid
+from pyld import jsonld
+
+
+class ApplicationProfile():
+    # Schemas, maps from term (or: alias) to schema IRI
+    schemas = dict()
+
+    # Mappings, from term to the term definition
+    mappings = dict()
+
+    id_to_term = dict()
+
+    def __init__(self, data):
+        context = data['@context']
+
+        # Mapping objects from the pyld package help us parse the CURIEs and more
+        self.mappings.update(self._get_mappings_from_context(data))
+        self.id_to_term = { v['@id']: k for k, v in self.mappings.items() }
+
+        # All terms that start with http:// or https:// are schemas
+        # This needs to be done first so that all schemas are available when we loop over the terms
+        self.schemas = {k: v for k, v in context.items() if type(context[k]) == str and (
+            context[k].startswith('http://') or context[k].startswith('https://'))}
+
+    def curie_to_full_iri(self, curie):
+        return self.mappings[curie]['@id']
+
+    def full_iri_to_curie(self, iri):
+        return next(k for k, n in self.mappings.items() if n['@id'] == iri)
+
+    def get_terms_per_schema(self):
+        terms_per_schema = dict()
+
+        for schema in self.schemas.keys():
+            terms_per_schema[schema] = list()
+
+        terms_per_schema['unknown'] = list()
+
+        for term, definition in self.mappings.items():
+            if term not in self.mappings:
+                terms_per_schema['unknown'].append(term)
+                continue
+
+            schema_with_longest_overlap = ''
+            for alias, iri in self.schemas.items():
+                if self.mappings[term]['@id'].startswith(iri) and (schema_with_longest_overlap == '' or len(iri) > len(self.schemas[schema_with_longest_overlap])):
+                    schema_with_longest_overlap = alias
+
+            if schema_with_longest_overlap == '':
+                terms_per_schema['unknown'].append(term)
+                continue
+
+            terms_per_schema[schema_with_longest_overlap].append(term)
+
+        if len(terms_per_schema['unknown']) == 0:
+            del terms_per_schema['unknown']
+
+        return terms_per_schema
+
+
+    def _get_mappings_from_context(self, data):
+        active_ctx = {'_uuid': str(
+            uuid.uuid1()), 'processingMode': 'json-ld-1.1', 'mappings': {}}
+        return jsonld.JsonLdProcessor().process_context(active_ctx, data, {})['mappings']
---
+++ software/config.py
@@ -0,0 +1,39 @@
+input = dict(
+  schemas=list(['../schemas', '../schema_ext-indischerfgoed.ttl']),
+  application_profile='../jsonldcontext.jsonld'
+)
+
+output = dict(
+    folder='./doc'
+)
+
+meta = dict(
+    title='PIED Model',
+    description='...',
+)
+
+language = dict(
+    CLASSES='Classes',
+    CLASS='Class',
+    PROPERTIES='Properties',
+    INSTANCES='Instanties',
+    GO_BACK='Ga terug',
+    INSTANCES_OF='Instanties van',
+    MAY_HAVE_THE_FOLLOWING_PROPERTIES='kunnen de volgende properties hebben',
+    SUBCLASSES_OF='Sub-classes van',
+    MAY_APPEAR_AS_VALUE='kunnen als waarde voor de volgende properties voorkomen',
+    PROPERTY='Property',
+    ON_TYPE='Op type',
+    DESCRIPTION='Beschrijving',
+    MORE_SPECIFIC_TYPES='Specifiekere types',
+    THESE_CLASSES_ARE_SUBCLASSES_OF='Deze classes zijn sub-classes vas',
+    DOMAIN='Domain',
+    PROPERTY_USED_ON_THESE_TYPES='Deze property komt voor op de volgende types',
+    DOMAIN_INCLUDES='Domain includes',
+    RANGE='Range',
+    VALUES_ARE_OF_THESE_TYPES='Waardes zijn van deze types',
+    RANGE_INCLUDES='Range includes',
+    EXPECTED_TYPE='Verwacht type',
+    THE_BASE_IRI_IS='De IRI van dit schema is',
+    NO_DIRECT_PROPERTIES='Deze class heeft geen directe properties',
+)
