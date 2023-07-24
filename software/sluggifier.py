4c82d36cd600c442cd26567ff0c5bafe6ece9113
Olivier Jansen <olivier@spinque.com>
2023-07-24 10:55:21 +0200

    testing new slugs

--- software/sluggifier.py
+++ software/sluggifier.py
@@ -12,21 +12,28 @@ class Sluggifier:
 
     def __init__(self, application_profile: ApplicationProfile):
         self.slugs = dict()
+        self.application_profile = application_profile
 
-        for term in application_profile.mappings.values():
-            slug = self._find_slug(term['@id'], self.slugs.values())
-            self.slugs[term['@id']] = slug
+        # for term in application_profile.mappings.values():
+        #     slug = self._find_slug(term['@id'], self.slugs.values())
+        #     self.slugs[term['@id']] = slug
+
 
     def transform(self, iri: str) -> str:
-        if iri in self.slugs:
-            return f'{self.slugs[iri]}.html'
+        # if iri in self.slugs:
+        #     return f'{self.slugs[iri]}.html'
+        # else:
+        #     return iri
+
+        if iri in self.application_profile.id_to_term:
+            return f'{self.application_profile.id_to_term[iri]}'
         else:
             return iri
 
-    def _find_slug(self, not_a_slug: str, existing_slugs: list[str]) -> str:
-        slug = slugify(not_a_slug)
-        i = 0
-        while slug in existing_slugs:
-            i += 1
-            slug = slugify(not_a_slug) + '-' + str(i)
-        return slug
+    # def _find_slug(self, not_a_slug: str, existing_slugs: list[str]) -> str:
+    #     slug = slugify(not_a_slug)
+    #     i = 0
+    #     while slug in existing_slugs:
+    #         i += 1
+    #         slug = slugify(not_a_slug) + '-' + str(i)
+    #     return slug
