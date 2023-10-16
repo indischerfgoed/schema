82a02bade90107268a5e2a867e67c99388b2d560
Michiel Hildebrand <michiel@spinque.com>
2023-10-16 11:17:58 +0200

    Delete software directory
    Use repo from spinque

--- software/utils.py
+++
@@ -1,9 +0,0 @@
-def flatten(t: list[list[any]]) -> list[any]:
-    return [item for sublist in t for item in sublist]
-
-
-def write_to_file(filepath: str, contents: str):
-    with open(filepath, 'w') as f:
-        f.seek(0)
-        f.write(contents)
-        f.truncate()
